35ae7c87df07b898aaaea3cccb558d0c589008a2
Jonnie Hallman <jonnie@destroytoday.com>
2018-04-28 15:44:50 -0400

    Added Firebase

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Firebase from 'firebase'
+import App from './app.vue'
 
 Vue.config.productionTip = false
+
+Firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+})
 
 new Vue({
   render: h => h(App)
